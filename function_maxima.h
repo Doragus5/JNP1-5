289823c949fc7ba823cab4bd4f098df380979f9a
Michal Lange <micha.soczewicy@gmail.com>
2021-01-03 17:24:03 +0100

    Napisane co trzeba zrobic poczatkowo

--- function_maxima.h
+++ function_maxima.h
@@ -1,4 +1,24 @@
 #ifndef FUNCTION_MAXIMA_H
 #define FUNCTION_MAXIMA_H
-
+template <typename A, typename V>
+class FunctionMaxima
+{
+    private:
+    //TODO mapa po argumentach
+    //TODO mapa maximow po wartosciach
+    public:
+    //TODO konstruktor bezparametrowy
+    //TODO konstruktor kopiujacy Konrad
+    //TODO operator= Konrad 
+    V const& value_at(A const& a) const {
+        //TODO
+    }
+    void set_value(A const& a, V const& v){
+        //TODO
+    }
+    void erase(A const& a){
+        //TODO
+    }
+    //TODO typ point_type Konrad
+}
 #endif //FUNCTION_MAXIMA_H
