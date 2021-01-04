2a566b9c20397506091ed9f7156ae52334a05db5
Michal Lange <micha.soczewicy@gmail.com>
2021-01-04 15:18:29 +0100

    Ustalone kontenery

--- function_maxima.h
+++ function_maxima.h
@@ -1,24 +1,39 @@
 #ifndef FUNCTION_MAXIMA_H
 #define FUNCTION_MAXIMA_H
+
+#include<map>
+#include<set>
+#include<utility>
+
+using std::pair;
+using std::map;
+using std::set;
+
+
 template <typename A, typename V>
 class FunctionMaxima
 {
     private:
-    //TODO mapa po argumentach
+    map <A, V> function_map;
+    set<pair<V, A> > maxima_set;
     //TODO mapa maximow po wartosciach
     public:
-    //TODO konstruktor bezparametrowy
+    FunctionMaxima() = default;
     //TODO konstruktor kopiujacy Konrad
     //TODO operator= Konrad 
     V const& value_at(A const& a) const {
         //TODO
     }
     void set_value(A const& a, V const& v){
-        //TODO
+        
+        //function_map.insert_or_assign();
     }
     void erase(A const& a){
         //TODO
     }
     //TODO typ point_type Konrad
-}
+};
+
+
+
 #endif //FUNCTION_MAXIMA_H
